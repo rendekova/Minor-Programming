2936c43821206f41d7cbca09b21b93ff21f2cc68
Martina Rendekova <martinarendekova@Martinas-MacBook-Air-2.local>
2025-12-16 22:13:56 -0600

    Fixed file structure and updated navigation links

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template, request
 import re
 import string
-# from database import Database # 
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-
-# db = Database('vanity.db') 
-# db.init_db() 
-
-# ... (all your functions)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
         plate = request.form['plate']
-        # db.add_numberPlates(plate) 
         if is_valid(plate):
             return render_template('valid.html', plate=plate)
         else:
             return render_template('invalid.html')
 
-    # plates = db.get_numberPlates()
-    return render_template('index.html', plates=[]) # App now returns an empty list
+    return render_template('index.html', plates=[])
 
+@app.route('/projects')
+def projects():
+    return render_template('myprojects.html')
 
+@app.route('/about')
+def about():
+    return render_template('learnaboutme.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
